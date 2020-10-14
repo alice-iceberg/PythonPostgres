1a23671db68148bed618fd9dd51b319b398c473b
alice-iceberg <aliceblackwood123@gmail.com>
2020-10-14 18:41:58 +0900

    checking photo type and saving the photo

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import psycopg2
 import time
 import os
 import base64
+import pandas as pd
 
 _dir = f'images/{int(time.time() * 1000)}'
+PHOTO_TYPE = b'PHOTO'
+
 
 db_conn = psycopg2.connect(
     host='127.0.0.1',
@@ -14,10 +17,15 @@ db_conn = psycopg2.connect(
 )
 
 cur = db_conn.cursor(cursor_factory=psycopg2_extras.DictCursor)
-cur.execute('select "value" from "data"."2-3" where data_source_id=25 and "timestamp"=1602664147088;')
+
+
+def create_pandas_table(sql_query, database=db_conn):
+    table = pd.read_sql_query(sql_query, database)
+    return table
+
+
+cur.execute('select "value" from "data"."2-3" where data_source_id=25 and "timestamp"=1602664808218;')
 rows = cur.fetchall()
-
-
 
 if not os.path.exists(_dir):
     os.mkdir(_dir)
@@ -25,11 +33,14 @@ if not os.path.exists(_dir):
 if rows is not None:
     counter = 1
     for row in rows:
-        image = bytes(row['value'])
-        image = base64.b64decode(image)
-        with open(f'{_dir}/{counter}.png', 'wb+') as w:
-            w.write(image)
-        counter += 1
+        value_type = bytes(row['value'][-5:])  # check if the type is photo
+        if value_type == PHOTO_TYPE:
+            print("This is photo")
+            image = bytes(row['value'][13:-4])  # between the timestamp and type
+            image = base64.b64decode(image)
+            with open(f'{_dir}/{counter}.png', 'wb+') as w:
+               w.write(image)
+            counter += 1
 
 cur.close()
 db_conn.close()
